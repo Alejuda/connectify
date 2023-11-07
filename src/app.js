78b4a932c1a0fbb0f248a8ea2c7ccc7eaf21e8b3
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-11-07 15:18:14 -0300

    Add auth routes and controllers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from 'dotenv';
 
+import authRoutes from "./routes/auth.routes.js";
+
 dotenv.config();
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(express.json())
+
+app.use('/api', authRoutes)
 
 export default app;
