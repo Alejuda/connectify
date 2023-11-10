c26ccff294c556eef687f4227b973751d386c3ba
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-11-10 10:54:17 -0300

    Add router and register route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+
 function App() {
   return (
-    <>
-    <h2 className="text-5xl">App</h2>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>Hola home page</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/posts' element={<h1>posts</h1>} />
+        <Route path='/add-post' element={<h1>create Post</h1>} />
+        <Route path='/post/:id' element={<h1>view post</h1>} />
+        <Route path='/profile' element={<h1>profile</h1>} />
+      </Routes>
+    </ BrowserRouter>
   )
 }
 
--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 import authRoutes from "./routes/auth.routes.js";
 import postsRoutes from './routes/posts.routes.js'
@@ -10,6 +11,9 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors({
+  origin: 'http://localhost:4000'
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
